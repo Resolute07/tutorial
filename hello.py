0a38471ad8fbe0306761e8575ae87e247dddf856
UTKARSH SINGH <utkarshsingh@UTKARSHs-MacBook-Air.local>
2023-02-19 19:46:59 +0530

    deleted some lines

--- hello.py
+++ hello.py
@@ -7,8 +7,6 @@ di = dict()
 for lin in hand:
     lin = lin.rstrip()
     #print(lin)
-    wds = lin.split()
-    #print(wds)
     for w in wds:
         # odiom: retrieve/create/update counter
         
